af254044a75c01f5d44acbfb9f67ef59258c858e
Guillermo Santos <guillermo.santosba@gmail.com>
2022-10-20 16:50:29 -0600

    Finished webpack + ts configuration.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.ts",
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			},
+		]
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "/dist")
+		}
+	},
+};
