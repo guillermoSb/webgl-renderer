bda4b8e5309271f48b5d9861813661f40ba42dc0
Guillermo Santos <guillermo.santosba@gmail.com>
2022-10-25 08:30:47 -0600

    Added 2d graphics

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,5 @@
+import { mat3, vec2 } from "gl-matrix";
+
 /**
  * Defines the structure of a Renderer
  */
@@ -18,55 +20,35 @@ export default class Renderer {
 		var canvas = document.querySelector("#renderer") as HTMLCanvasElement;
 		canvas.width = canvas.clientWidth;
 		canvas.height = canvas.clientHeight;
-		var gl = canvas.getContext("webgl");
+		let gl = canvas.getContext("webgl");
 		if (!gl) {
 			return;
 		}
-		// Get the strings for our GLSL shaders
-		const vertexShaderSource = (document.querySelector("#vertex-shader-2d") as HTMLScriptElement).text;
-		const fragmentShaderSource = (document.querySelector("#fragment-shader-2d") as HTMLScriptElement).text;
+		const program = this.initProgram(gl);	// Init WebGl Program
 
-		// create GLSL shaders, upload the GLSL source, compile the shaders
-		var vertexShader = this.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-		var fragmentShader = this.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-		// Link the two shaders into a program
-		var program = this.createProgram(gl, vertexShader, fragmentShader);
-
-		// look up where the vertex data needs to go.
-		var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
+		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+		
+		// Setup attributes and uniforms
+		let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+		let resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+		let matrixLocation = gl.getUniformLocation(program, "u_matrix");
 		// Create a buffer and put three 2d clip space points in it
 		var positionBuffer = gl.createBuffer();
-
 		// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
 		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-		var positions = [
-			0, 0,
-			0, 0.5,
-			0.7, 0,
-		];
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-		// code above this line is initialization code.
-		// code below this line is rendering code.
-
-
-		// Tell WebGL how to convert from clip space to pixels
-		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+		gl.enableVertexAttribArray(positionAttributeLocation);
+	
+		this.setFigure(gl);
+		let matrix = mat3.create();
+		mat3.translate(matrix, matrix, vec2.fromValues(100, 100));
+		mat3.rotate(matrix, matrix, Math.PI / 7);
+		mat3.scale(matrix, matrix, vec2.fromValues(1, 1));
+		
 
 		// Clear the canvas
 		gl.clearColor(0, 0, 0, 1);
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
-		// Tell it to use our program (pair of shaders)
-		gl.useProgram(program);
-
-		// Turn on the attribute
-		gl.enableVertexAttribArray(positionAttributeLocation);
-
-		// Bind the position buffer.
-		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
 		// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
 		var size = 2;          // 2 components per iteration
@@ -76,15 +58,30 @@ export default class Renderer {
 		var offset = 0;        // start at the beginning of the buffer
 		gl.vertexAttribPointer(
 			positionAttributeLocation, size, type, normalize, stride, offset);
+		gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+		gl.uniformMatrix3fv(matrixLocation, false, matrix);
 
 		// draw
 		var primitiveType = gl.TRIANGLES;
 		var offset = 0;
-		var count = 3;
+		var count = 18;
 		gl.drawArrays(primitiveType, offset, count);
 	}
 
-	createShader(gl: any, type: any, source: any) {
+	public initProgram(gl: WebGLRenderingContext): WebGLProgram {
+		// Get the strings for our GLSL shaders
+		const vertexShaderSource = (document.querySelector("#vertex-shader-2d") as HTMLScriptElement).text;
+		const fragmentShaderSource = (document.querySelector("#fragment-shader-2d") as HTMLScriptElement).text;
+		// create GLSL shaders, upload the GLSL source, compile the shaders
+		var vertexShader = this.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+		var fragmentShader = this.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+		// Link the two shaders into a program
+		var program = this.createProgram(gl, vertexShader, fragmentShader);
+		gl.useProgram(program);
+		return program;
+	}
+
+	public  createShader(gl: any, type: any, source: any) {
 		var shader = gl.createShader(type);
 		gl.shaderSource(shader, source);
 		gl.compileShader(shader);
@@ -97,7 +94,39 @@ export default class Renderer {
 		gl.deleteShader(shader);
 	}
 
-	createProgram(gl: any, vertexShader: any, fragmentShader: any) {
+
+	public setFigure(gl: WebGLRenderingContext) {
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+          // left column
+          0, 0,
+          30, 0,
+          0, 150,
+          0, 150,
+          30, 0,
+          30, 150,
+ 
+          // top rung
+          30, 0,
+          100, 0,
+          30, 30,
+          30, 30,
+          100, 0,
+          100, 30,
+ 
+          // middle rung
+          30, 60,
+          67, 60,
+          30, 90,
+          30, 90,
+          67, 60,
+          67, 90,
+      ]),
+      gl.STATIC_DRAW);
+	}
+	
+	public createProgram(gl: any, vertexShader: any, fragmentShader: any) {
 		var program = gl.createProgram();
 		gl.attachShader(program, vertexShader);
 		gl.attachShader(program, fragmentShader);
